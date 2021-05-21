f3edd64c949f84f8ee526c8263c85ca7838cdccd
Sunny78inator <43562714+prathyushSunny@users.noreply.github.com>
2021-05-21 16:28:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let loading = document.getElementById('loading');
 let main = document.getElementById('mainContent');
 let loadingPercentage = document.getElementById('loadingPercentage');
-let loadingPercentageMd = document.getElementById('loadingPercentageMd');
-console.log(loadingPercentageMd);
 let percentage = 0;
 document.body.removeChild(main);
 
@@ -11,12 +9,27 @@ percentageId = setInterval(function() {
     if (percentage > 100) {
         clearInterval(percentageId);
     } else {
-        loadingPercentage.textContent = "(Loading... " + percentage + "%)";
-        loadingPercentageMd.textContent = "(Loading... " + percentage + "%)";    
+        loadingPercentage.textContent = "(Loading... " + percentage + "%)";        
     }   
 }, 50);
 
 setTimeout(function() {
     document.body.removeChild(loading);
     document.body.appendChild(main);
+    // adding animations to the SLIDER on the left
+    var slider = document.getElementById('sectionSlider');
+    var sliderBtn = document.getElementById('sectionSliderButton');
+    console.log(sliderBtn);
+    let option1 = document.getElementById('option_1');
+    let option2 = document.getElementById('option_2');
+    sliderBtn.onclick = function(){
+    console.log('triggered!');
+    sliderBtn.classList.toggle('slider-button-animation');
+    slider.classList.toggle('slider-open-animation');
+    option1.classList.toggle('options-container-show');
+    option2.classList.toggle('options-container-show');
+}
 }, 6200);
+
+
+
